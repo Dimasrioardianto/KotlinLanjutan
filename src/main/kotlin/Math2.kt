da5a2d17ab7ceb55819d1cbff3159ab26a2169a3
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-16 14:40:25 +0700

    add Math2.kt

---
+++ src/main/kotlin/Math2.kt
@@ -0,0 +1,11 @@
+import kotlin.math.PI
+
+fun main (args: Array<String>) {
+    //Advance exercise
+    print("Enter the circle's radius (cm): ")
+    val radius = readLine()!!.toDouble()
+    var circumference = 2 * PI * radius
+    var area = PI * radius * radius
+    println("The circle's circumference based on the given radius is: " + circumference+ "cm")
+    println("Area of the cicle is: " + area + "cm^2")
+}
